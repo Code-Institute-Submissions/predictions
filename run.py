19125becbcd46307a0335641a70a36c478253272
arpreacher <anishrpillai@hotmail.com>
2022-07-17 16:37:19 +0000

    Importing libraries

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# numpy is used for array containers
+import numpy as np
+import pandas as pd 
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn import svm
