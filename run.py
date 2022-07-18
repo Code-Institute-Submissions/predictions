c2ab953366c89ba5521a1ee29239a03255ed7a83
arpreacher <anishrpillai@hotmail.com>
2022-07-18 12:41:20 +0000

    Data columns with str values converted to int

--- run.py
+++ run.py
@@ -5,17 +5,62 @@ from sklearn import svm
 # Data loading
 print("Welcome to lender assistant")
 print("Loading history data")
-loadDataset = pandas.read_csv('loan_dataset.csv')
-print(loadDataset.head())
+loanDataset = pandas.read_csv('loan_dataset.csv')
+print(loanDataset.head())
 
 print("Preprocessing data")
 # Drops rows with missing values from the dataset
-loadDataset = loadDataset.dropna()
+loanDataset = loanDataset.dropna()
 
 # using Intenum to replace Strings with integer values
 class LoanStatus(IntEnum):
     REJECTED = 0
     ACCEPTED = 1
-loadDataset.replace({"Loan_Status": {'N': LoanStatus.REJECTED.value, 'Y': LoanStatus.ACCEPTED.value}}, inplace=True)
-print(loadDataset.head())
-print(loadDataset['Loan_Status'].value_counts())
+loanDataset.replace({"Loan_Status": {'N': LoanStatus.REJECTED.value, 'Y': LoanStatus.ACCEPTED.value}}, inplace=True)
+print(loanDataset.head())
+print(loanDataset['Loan_Status'].value_counts())
+
+class Dependents(IntEnum):
+    ZERO = 0
+    ONE = 1
+    TWO = 2
+    MANY = 3
+loanDataset.replace({"Dependents": {'3+': Dependents.MANY.value}}, inplace=True)
+print(loanDataset.head())
+print(loanDataset['Dependents'].value_counts())
+
+class PropertyArea(IntEnum):
+    URBAN = 0
+    SEMI_URBAN = 1
+    RURAL = 2
+loanDataset.replace({"Property_Area": {'Urban': PropertyArea.RURAL.value}}, inplace=True)
+print(loanDataset.head())
+print(loanDataset['Property_Area'].value_counts())
+
+class Gender(IntEnum):
+    MALE = 0
+    FEMALE = 1
+loanDataset.replace({"Gender": {'Male': Gender.MALE.value, 'Female': Gender.FEMALE.value}}, inplace=True)
+print(loanDataset.head())
+print(loanDataset['Gender'].value_counts())
+
+class Education(IntEnum):
+    GRADUATE = 1
+    NOT_GRADUATE = 0
+loanDataset.replace({"Education": {'Graduate': Education.GRADUATE.value,'Not Graduate': Education.NOT_GRADUATE.value}}, inplace=True)
+print(loanDataset.head())
+print(loanDataset['Education'].value_counts())
+
+class Married(IntEnum):
+    SINGLE = 0
+    MARRIED = 1
+loanDataset.replace({"Married": {'No': Married.SINGLE.value, 'Yes': Married.SINGLE.value}}, inplace=True)
+print(loanDataset.head())
+print(loanDataset['Married'].value_counts())
+
+class SelfEmployed(IntEnum):
+    SELF_EMPLOYED = 0
+    NOT_SELF_EMPLOYED = 1
+loanDataset.replace({'Self_Employed': {'No': SelfEmployed.SELF_EMPLOYED.value, 'Yes': SelfEmployed.SELF_EMPLOYED.value}}, inplace=True)
+print(loanDataset.head())
+print(loanDataset['Self_Employed'].value_counts())
