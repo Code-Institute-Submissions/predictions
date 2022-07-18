8d32392da0823e497d9eb16cdb734b13f53fd186
arpreacher <anishrpillai@hotmail.com>
2022-07-18 11:45:41 +0000

    Replace string with integer values for easy processing

--- run.py
+++ run.py
@@ -7,3 +7,15 @@ print("Welcome to lender assistant")
 print("Loading history data")
 loadDataset = pandas.read_csv('loan_dataset.csv')
 print(loadDataset.head())
+
+print("Preprocessing data")
+# Drops rows with missing values from the dataset
+loadDataset = loadDataset.dropna()
+
+# using Intenum to replace Strings with integer values
+class LoanStatus(IntEnum):
+    REJECTED = 0
+    ACCEPTED = 1
+loadDataset.replace({"Loan_Status": {'N': LoanStatus.REJECTED.value, 'Y': LoanStatus.ACCEPTED.value}}, inplace=True)
+print(loadDataset.head())
+print(loadDataset['Loan_Status'].value_counts())
